80162bcfaf9e01b2cbe1df25fc46c0dd865a2be1
Sean A <essefx@gmail.com>
2023-10-22 15:29:04 +0700

    Sun #2 updated examples/includes.php

--- examples/includes.php
+++ examples/includes.php
@@ -3,13 +3,5 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 date_default_timezone_set('Asia/Jakarta');
-define('TIMER_START', microtime(true));
 
 require_once __DIR__ . '/../vendor/autoload.php';
-
-// Dotenv
-$env = __DIR__ .'/../.env';
-if (file_exists($env)) {
-	$dotenv = new Symfony\Component\Dotenv\Dotenv();
-	$dotenv->load($env);
-}
